e49f64d721d25c4ae1866836a7b1f056ecab64be
lovehackerAsif <61407838+lovehackerAsif@users.noreply.github.com>
2020-03-19 15:23:03 +0500

    Create lov3.py

---
+++ lov3.py
@@ -0,0 +1,192 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+ 
+# Jangan Recode Ya gayn
+ 
+echo
+echo
+echo
+    echo $i"       [•]"$me"─────────────────────────────────────────── "$i"[•]"
+     echo $i"        |"$cy"        Pakistan  DARK  TERMUX ASSOCIATE      "$i"|"
+     echo $i"        |"$me"───────────────────────────────────────────── "$i"|"
+     echo $i"        |"$pu"    Auther :"$bi" lovehacker....                   "$i"|"
+     echo $i"        |"$pu"    Facebook :"$bi" Ali Ahmad......                "$i"|"
+     echo $i"        |"$pu"    Contact WhatsApp :"$bi" 03094161457            "$i"|"
+     echo $i"        |"$pu"   Contact Gmail :"$bi" Frends Subscrib My Chenal  "$i"|"
+    echo $i"       [•]"$me"────────────────────────────────────────────"$i"[•]"
+echo
+ 
+ 
+echo
+echo
+echo
+echo $cy"       ░█▀▀"$i"░░░█▀█░░░"$me"█▀▀░░░"$ku"█▀▀░░░"$cy"█▀▄░░░"$i"█▀█░░░"$bi"█▀█░░░"$pur"█░█"
+echo $bi"       ░█▀▀░░░"$cy"█▀█░░░█░░░░░"$bi"█▀▀░░░"$i"█"$cy"▀▄░░░█░"$me"█░░░█░█░░░"$ku"█▀▄"
+echo $pur"       ░▀░░░░░▀░▀░░░▀"$bi"▀▀░░░▀▀"$i"▀░░░▀▀░░░░"$me"▀▀▀░░░"$cy"▀▀▀░░░▀░"$bi"▀"
+echo
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+echo
+echo
+echo
+      echo $ku"        ★"$i"##########################################"$ku"★"
+      echo $ku"        ★"$cy"   |"$me"1"$cy"|"$pur"  Facebook Auto Tools osif"$ku"               ★"
+      echo $ku"        ★"$cy"   |"$me"2"$cy"|"$pur"  Facebook Hack"$i" [Kali.linux]"$ku"          ★"
+      echo $ku"        ★"$cy"   |"$me"3"$cy"|"$pur"  Facebook Hack"$i" [Black.Mafia]"$ku"        ★"
+      echo $ku"        ★"$cy"   |"$me"4"$cy"|"$pur"  Facebook Auto Report"$ku"              ★"
+      echo $ku"        ★"$i"##########################################"$ku"★"
+echo
+    
+ 
+ 
+echo
+echo
+echo
+echo $i"        [5] Install All Packages" 
+echo $me"        [0] EXIT"
+echo
+    
+ 
+ 
+echo $me"╔═══"$bi"["$i"loveHacker"$bi"]"$me"══════"$bi"["$i""loveHacker""$bi"]"
+echo $me"║"
+read -p"╚═══➣➣ " slect
+ 
+ 
+if [ $slect = 1 ]
+then
+clear
+echo
+echo
+echo
+echo $pur"░█░█░░░█▀█░░░▀█▀░░░▀█▀
+░█▄█░░░█▀█░░░░█░░░░░█░
+░▀░▀░░░▀░▀░░░▀▀▀░░░░▀░"
+echo
+sleep 1
+git clone https://github.com/ciku370/OSIF
+cd OSIF	
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+ 
+if [ $slect = 2 ]
+then
+clear
+echo
+echo
+echo
+echo $cy"░█░█░░░█▀█░░░▀█▀░░░▀█▀
+░█▄█░░░█▀█░░░░█░░░░░█░
+░▀░▀░░░▀░▀░░░▀▀▀░░░░▀░"
+echo
+sleep 1
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/lovehackerAsif/Kali.linux/
+cd Kali.linux
+python2 NetHuntter
+fi
+ 
+if [ $slect = 3 ]
+then
+clear
+echo
+echo
+echo
+echo $i"░█░█░░░█▀█░░░▀█▀░░░▀█▀
+░█▄█░░░█▀█░░░░█░░░░░█░
+░▀░▀░░░▀░▀░░░▀▀▀░░░░▀░"
+echo
+sleep 1
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/lovehackerAsif/BlackMafia/
+cd BlackMafia
+python2 BlackMafia.py
+fi
+ 
+if [ $slect = 5 ]
+then
+clear
+apt update && apt upgrade
+apt install python2 -y
+pkg install python3
+pkg install python2 -y
+pkg install python
+pkg install git -y
+pip2 install urllib3 chardet certifi idna requests
+pkg install openssh -y
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install php -y
+pkg install ruby cowsay toilet figlet -y
+pkg install neofetch
+pkg install nano
+toilet -f pagga "S U C C E S S" | lolcat
+fi
+ 
+ 
+if [ $slect = 0 ]
+then
+clear
+echo
+echo
+echo
+echo $me"    ______   _  __    ____   ______
+   / ____/  | |/ /   /  _/  /_  __/
+  / __/     |   /    / /     / /
+ / /___    /   |   _/ /     / /
+/_____/   /_/|_|  /___/    /_/"
+echo
+sleep 2
+echo $cy" Thanks for support us "
+sleep 2
+exit
+fi
+ 
+if [ $slect = 4 ]
+then
+clear
+echo
+echo
+echo
+echo $bi"░█░█░░░█▀█░░░▀█▀░░░▀█▀
+░█▄█░░░█▀█░░░░█░░░░░█░
+░▀░▀░░░▀░▀░░░▀▀▀░░░░▀░"
+echo
+sleep 1
+git clone https://github.com/IlayTamvan/Report 
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+ 
+ 
+ 
+ 
